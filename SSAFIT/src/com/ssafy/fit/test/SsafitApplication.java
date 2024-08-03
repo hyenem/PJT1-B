8846937b93f6fccab11331522c4dcf205fbd598d
hyenem <gpals04291@gmail.com>
2024-08-03 23:58:17 +0900

    0802

--- SSAFIT/src/com/ssafy/fit/test/SsafitApplication.java
+++ SSAFIT/src/com/ssafy/fit/test/SsafitApplication.java
@@ -8,14 +8,14 @@ import com.ssafy.fit.ui.VideoReviewUi;
 public class SsafitApplication {
 
 	public static void main(String[] args) {
-//		UserManagerImpl usermanager = UserManagerImpl.getInstance();
-//		usermanager.loadUserData();
-//		
-//		MainUi main = new MainUi();
-//		main.service();
+		UserManagerImpl usermanager = UserManagerImpl.getInstance();
+		usermanager.loadUserData();
 		
-		VideoReviewUi reviewUi = VideoReviewUi.getInstance(0);
-		reviewUi.service();
+		MainUi main = new MainUi();
+		main.service();
+		
+//		VideoReviewUi reviewUi = VideoReviewUi.getInstance(0);
+//		reviewUi.service();
 	}
 
 }
