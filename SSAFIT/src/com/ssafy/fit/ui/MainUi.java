7a84063678bcbea7b3eb928fe40b8fc25347ba8c
hyenem <gpals04291@gmail.com>
2024-08-03 14:14:48 +0900

    0802

--- SSAFIT/src/com/ssafy/fit/ui/MainUi.java
+++ SSAFIT/src/com/ssafy/fit/ui/MainUi.java
@@ -1,5 +1,15 @@
 package com.ssafy.fit.ui;
 
-public class MainUi {
+import com.ssafy.fit.model.dao.IUserManager;
+import com.ssafy.fit.model.dao.UserManagerImpl;
 
+public class MainUi {
+	public void service() {
+		System.out.println("-------SSAFIT과 함께 건강을 지켜요!-------");
+	}
+	
+	public void exit() {
+		IUserManager userDao = UserManagerImpl.getInstance();
+		userDao.
+	}
 }
